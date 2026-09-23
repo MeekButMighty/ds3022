a42304e470de4c519c6e71c200451640a917c02a
oliver <oliver.meek2@gmail.com>
2026-09-23 10:55:22 -0400

    learned about apis

--- 04_apis/01_plain.py
+++ 04_apis/01_plain.py
@@ -4,7 +4,7 @@
 import httpx
 import json
 
-USER = "schaconxyz"
+USER = "MeekButMighty"
 URL = "https://api.github.com/users/{user}/events/public"
 
 response = httpx.get(URL.format(user=USER))
@@ -12,4 +12,5 @@ response = httpx.get(URL.format(user=USER))
 data = response.json()
 
 for item in data:
-  print(item["repo"]["name"], " - ", item["type"])
+	print(item['repo']['name'], " - ", item['type'])
+
--- 04_apis/02_errors.py
+++ 04_apis/02_errors.py
@@ -7,14 +7,15 @@ import json
 USER = "schaconx"
 URL = "https://api.github.com/users/{user}/events/public"
 
-try:
-  response = httpx.get(URL.format(user=USER))
-  response.raise_for_status()
-  data = response.json()
 
-  for item in data:
-    print(item["repo"]["name"], " - ", item["type"])
+try:
+	response = httpx.get(URL.format(user=USER))
+	response.raise_for_status()
+	data = response.json()
+
+	for item in data:
+		print(item['repo']['name'], " - ", item['type'])
 
 except httpx.HTTPError as e:
-  print(e)
+	print(e)
 
